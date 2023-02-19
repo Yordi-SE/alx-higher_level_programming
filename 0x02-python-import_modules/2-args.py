4d295dc77c7409f749af893f80997f195ca5c064
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-19 12:27:44 -0800

    command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def main():
+    import sys
+    if len(sys.argv) == 2:
+        print("{} argument:".format(1))
+        print("1: {}".format(sys.argv[1]))
+    elif len(sys.argv) == 1:
+        print("{} arguments.".format(0))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[1]))
+
+
+if __name__ == "__main__":
+    main()
