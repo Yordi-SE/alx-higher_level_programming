cfe097304f5f316ebac5c3f5a7d853ef95db0747
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-20 04:03:10 -0800

    7-add_tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,8 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
+    if (len(tuple_a) == len(tuple_b) == 0):
+        tuple_c = 0, 0
     if (len(tuple_a) == 1):
         tuple_c = tuple_a[0] + tuple_b[0], tuple_b[1] + 0
     elif (len(tuple_b) == 1):
