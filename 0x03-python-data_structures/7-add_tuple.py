8d6fdccb1b258db9cc600af8648629fbff960d62
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-20 06:30:29 -0800

    multiple returns

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,16 +2,7 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if (len(tuple_a) == len(tuple_b) == 0):
-        tuple_c = 0, 0
-    elif (len(tuple_a) == 1):
-        tuple_c = tuple_a[0] + tuple_b[0], tuple_b[1] + 0
-    elif (len(tuple_b) == 1):
-        tuple_c = tuple_b[0] + tuple_a[0], tuple_a[1] + 0
-    elif len(tuple_b) == 0:
-        tuple_c = tuple_a[0], tuple_a[1]
-    elif len(tuple_a) == 0:
-        tuple_c = tuple_b[0], tuple_b[1]
-    else:
-        tuple_c = tuple_b[0] + tuple_a[0], tuple_a[1] + tuple_b[1]
-    return tuple_c
+    tuple_c = tuple_a + (0, 0)
+    tuple_d = tuple_b + (0, 0)
+    tuple_e = tuple_c[0] + tuple_d[0], tuple_c[1] + tuple_d[1]
+    return tuple_e
