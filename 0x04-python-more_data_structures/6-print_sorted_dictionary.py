e3a93b82d1cbd1718a70def803f2bdfed490eb7b
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-21 05:03:24 -0800

    sort dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_dict = dict(sorted(a_dictionary.items()))
+    for i , v in sorted_dict.items():
+        print("{}: {}".format(i, v))
