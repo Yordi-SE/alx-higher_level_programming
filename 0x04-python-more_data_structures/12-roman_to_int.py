bf5035ed3872e7ed67c9d740c656f34a7d93fd1a
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-21 08:03:19 -0800

    roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,11 +4,15 @@
 def roman_to_int(roman_string):
     my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
+    list1 = list(roman_string)
     if roman_string:
-        for i in my_dict.keys():
-            for j in roman_string:
-                if (i == j):
-                    num = num + my_dict[i]
-        return num
-    else:
-        return 0
+        for i, j in enumerate(list1):
+            if (j == 'I' and (i + 1) < len(list1) and list1[i+1] != 'I'):
+                if (list1[i+1] == 'V' or list1[i+1] == 'X'):
+                    num = num + (my_dict[list1[i + 1]] - 1)
+                    del list1[i+1]
+                else:
+                    continue
+            else:
+                num = num + my_dict[j]
+    return num
