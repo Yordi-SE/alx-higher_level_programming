8a8ea4b935ccecad045c3c87c2554d5fde7116c6
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-22 03:25:39 -0800

    safe print list integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    num = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num += 1
+        except ValueError:
+            pass
+        except TypeError:
+            pass
+    if (x > 0):
+        print("")
+    return num
