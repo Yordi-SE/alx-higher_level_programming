227167581a2acdbdb36c31301a1a6272d92cb813
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-22 04:07:28 -0800

    safe print list integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,11 +7,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             num += 1
-        except OverflowError:
-            continue
-        except ValueError:
-            continue
-        except TypeError:
+        except IndexError:
+            raise
+        except Exception:
             continue
     if (x > 0):
         print("")
