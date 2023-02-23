8f1d0717e707cea8eb9167a7e78ef49b62f76d61
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-23 09:03:59 -0800

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" It is empty class Square that defines a square"""
+
 class Square:
-    """ It is empty class Square that defines a square
-    It is empy class Square that defines a square
-    """
+
     pass
+
+    """ class has defined """
