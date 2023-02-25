40d6952598e69a3c48b8a882bb48ec7893f10c31
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-25 13:33:54 -0800

    addition function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""addition function
+    This Module contains the defination of
+    function that used to add integers
+"""
+
+
+def add_integer(a, b=98):
+    """Here we define function that return sum of its arguments
+    """
+
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    """Here we cast floats to integers if any
+    """
+    x, y = (int(i) for i in [a, b])
+    return x + y
+
+    
