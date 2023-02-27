93a25ff28a74320f9467a7f18bc7a69df57cb99a
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-27 01:35:54 -0800

    rectangle class

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -7,3 +7,7 @@ print(my_rectangle.__dict__)
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
+m3 = Rectangle(1, 2.3)
+print(m1.height)
+print(m2.width)
+print(m3.width, m3.height)
