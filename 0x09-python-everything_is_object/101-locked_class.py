6efac932e01885a487bd6fa339be0e68748d5c5e
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-28 06:35:38 -0800

    locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contanis the defination of
+    LockedClass module
+    """
+
+
+class LockedClass:
+    """Here we define the class
+    """
+
+    __slots__ = ["first_name"]
