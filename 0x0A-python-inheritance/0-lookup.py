0b8ef5ab8dde3de0b5c09d3428db871c8243209d
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-02-28 23:40:12 -0800

    let"s lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is the module that contains the
+    defination of function that list attributes
+    and methods of class instance
+    """
+
+
+def lookup(obj):
+    """Over here we define the function
+    """
+    return dir(obj)
