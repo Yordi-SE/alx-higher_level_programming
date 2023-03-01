504e4043760826ead7a1029a647134680d3bb6ea
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-03-01 03:28:25 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define an empty class
+BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """doc for the class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
