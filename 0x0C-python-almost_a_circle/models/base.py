ac4dd87941dba69d11bd283b6f7505dce7a16321
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-03-03 12:52:57 -0800

    base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains the defination
+of the class Base
+"""
+
+
+class Base:
+    """Here  body of the
+    class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Here we define the constructor
+        of this class
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            id = Base.__nb_objects
