ba1259a737d00ef35393c89d53fa42b63d6dada1
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-03-02 21:54:37 -0800

    to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains the defination of
+ function that returns JSON representation
+ """
+import json
+
+
+def to_json_string(my_obj):
+    """Here is the body of
+    the function
+    """
+    return json.dumps(my_obj)
