c12fb6637072d731d94c0459a2ef653f2a6672b4
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2023-03-04 02:54:27 -0800

    modified rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,10 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """This is puplic instance method
+        that returns the area of the 
+        rectangle
+        """
+        return self.width * self.height
