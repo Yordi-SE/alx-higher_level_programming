8cd67053777842563976a808e93ad55b2a474af4
Yordanos Lemmawork <yordanoslemmaworkofkind1@gmail.com>
2023-03-12 06:09:10 -0700

    come and see Dexter's genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,5 +2,5 @@
 SELECT tv_genres.name FROM tv_shows
 JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = "Dexter";
-ORDER BY tv_genres.name ASC
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
