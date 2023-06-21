6410b61b6ce9f59647fbe4be3e70c9c28adbe20f
Yordanos Lemmawork <yordanoslemmaworkofkind1@gmail.com>
2023-06-21 08:14:38 -0700

    mysql python

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" this excute the database to return all columns
+of states table"""
+if __name__ == '__main__':
+    import MySQLdb
+    h = 'localhost'
+    r = 'root'
+    mdb = 'hbtn_0e_0_usa'
+    db = MySQLdb.connect(host=h, port=3306, user=r, passwd=r, db=mdb)
+    cur = db.cursor()
+    cur.execute("select * from states where name like 'N%' order by id")
+    rows = cur.fetchall()
+    for row in rows:
+        print("(", end="")
+        for col in row:
+            if not isinstance(col, int):
+                print("'" + col + "'", end="")
+            else:
+                print(col, end=", ")
+        print(")")
