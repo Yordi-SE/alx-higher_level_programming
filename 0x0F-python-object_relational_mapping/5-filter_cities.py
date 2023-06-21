dadcce70e7d5aaff566aabc16ac2065c2f99c2a5
Yordanos Lemmawork <yordanoslemmaworkofkind1@gmail.com>
2023-06-21 11:20:11 -0700

    python mysql

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" this excute the database to return all columns
+of states table"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    h = 'localhost'
+    r = sys.argv[1]
+    m = sys.argv[2]
+    mdb = sys.argv[3]
+    s = sys.argv[4]
+    db = MySQLdb.connect(host=h, port=3306, user=r, passwd=m, db=mdb)
+    query = "select cities.name from cities inner join states\
+            on cities.state_id = states.id where states.name = %s\
+            order by cities.id ASC;"
+    cur = db.cursor()
+    cur.execute(query, (s,))
+    rows = cur.fetchall()
+    for i in range(len(rows)):
+        if i < len(rows) - 1:
+            print(rows[i][0], end=", ")
+        else:
+            print(rows[i][0])
