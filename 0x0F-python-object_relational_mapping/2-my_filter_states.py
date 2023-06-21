b7bd6b2149f9740561f98e67695699c5ffe42f6e
Yordanos Lemmawork <yordanoslemmaworkofkind1@gmail.com>
2023-06-21 08:54:11 -0700

    python mysql

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" this excute the database to return all columns
+of states table"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    h = 'localhost'
+    r = sys.argv[1]
+    m = sys.argv[2]
+    mdb = sys.argv[3]
+    s = sys.argv[4]
+    db = MySQLdb.connect(host=h, port=3306, user=r, passwd=m, db=mdb)
+    cur = db.cursor()
+    cur.execute("select * from states;")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == s:
+            print(row)
