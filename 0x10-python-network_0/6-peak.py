d2ceeed53e466f1012e846ae9c4339200ea27d9a
Yordi SE <yordanoslemmaworkofkind1@gmail.com>
2023-07-05 13:19:35 -0700

    python

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""finds a peak in a list of unsorted integers"""
+"""Defines a function that finds the peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """here we write
-    body of the function"""
+    """Finds the peak in a list of unsorted integers
+    Args:
+        list_of_integers (list): Unsorted list of integers to find peak of.
+    Returns:
+        int: The peak of the unsorted list.
+    """
     length = len(list_of_integers)
+
     if length == 0:
         return None
-    lis = sorted(list_of_integers)
-    return lis[length - 1]
+    elif length == 1:
+        return list_of_integers[0]
+    elif length == 2:
+        return max(list_of_integers)
+
+    list_of_integers.sort(reverse=True)
+
+    return list_of_integers[0]
