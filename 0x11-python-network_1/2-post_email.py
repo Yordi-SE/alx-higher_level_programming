5d639f552cc58b1b2f0c9035fa9f56ca5e2ecbbc
Yordi SE <yordanoslemmaworkofkind1@gmail.com>
2023-07-13 03:15:49 -0700

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ import urllib.parse
 
 
 def main():
-    email = {'email' : sys.argv[2]}
+    email = {'email':sys.argv[2]}
     email = urllib.parse.urlencode(email)
     email = email.encode('ascii')
     url = sys.argv[1]
@@ -16,6 +16,7 @@ def main():
     with urllib.request.urlopen(req) as f:
         body = f.read()
         print(body.decode('utf-8'))
+
 
 if __name__ == '__main__':
     main()
