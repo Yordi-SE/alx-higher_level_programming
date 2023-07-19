eca4551b298fe705dc0ef6e4cf5159db26c4a15d
Yordi SE <yordanoslemmaworkofkind1@gmail.com>
2023-07-19 10:02:03 -0700

    first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a script that lists all State objects from
+the database
+"""
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+if __name__ == '__main__':
+    url = "mysql://{}:{}@localhost:3306/{}".format(sys.argv[1],
+                                                   sys.argv[2], sys.argv[3])
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    row = session.query(State).first()
+    if row is None:
+        print("Nothing")
+    else:
+        print("{}:".format(row.id), row.name)
