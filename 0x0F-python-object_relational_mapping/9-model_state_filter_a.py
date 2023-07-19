e548d632e03bc215eb5c0343e383dfe820de2f03
Yordi SE <yordanoslemmaworkofkind1@gmail.com>
2023-07-19 10:46:49 -0700

    Texas

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a script that lists all State objects from
+the database
+"""
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+if __name__ == '__main__':
+    url = "mysql://{}:{}@localhost:3306/{}".format(sys.argv[1],
+                                                   sys.argv[2], sys.argv[3])
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for row in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+        print("{}:".format(row.id), row.name)
